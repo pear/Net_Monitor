2925b7adbdc4c0abd2998a348d7d3916e1001549
Robert Peake <cyberscribe@php.net>
2005-03-29 06:07:11 +0000

    Minor update to provide compatability with new PHP header DocBlock format
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_Monitor/trunk@183099 c90b9560-bf6c-de11-be94-00142212c4b1

--- Monitor/Alert/SMS.php
+++ Monitor/Alert/SMS.php
@@ -1,29 +1,28 @@
 <?php
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2005 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/3_0.txt.                                  |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Author: Robert Peake <robert@peakepro.com>                           |
-// +----------------------------------------------------------------------+
-//
-// $Id$
-//
-// Remote service monitor alerter thru SMS
 /**
- * @package Net_Monitor
- * @author Robert Peake <robert@peakepro.com>
- * @copyright 2005
- * @license http://www.php.net/license/3_0.txt
- * @version 0.2.0
+ * Network service monitoring package
+ *
+ * A unified interface for checking the availability services on external 
+ * servers and sending meaningful alerts through a variety of media if a 
+ * service becomes unavailable.
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Net
+ * @package    Net_Monitor
+ * @author     Robert Peake <cyberscribe@php.net>
+ * @author     Bertrand Gugger <bertrand@toggg.com>
+ * @copyright  2004-2005 Bertrand Gugger
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/PackageName
+ * @since      File available since Release 0.0.6
  */
 /**
  * requires and extends the Net_Monitor_Alert class
@@ -36,6 +35,7 @@ require_once 'Net/SMS.php';
 /** 
  * class Net_Monitor_Alert_SMS
  *
+ * @category Net
  * @package Net_Monitor
  * @access public
  * @see Net_Monitor_Alert
--- Monitor/Service/DNS.php
+++ Monitor/Service/DNS.php
@@ -1,31 +1,29 @@
 <?php
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2004 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/3_0.txt.                                  |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Author: Robert Peake <robert@peakepro.com>                           |
-// +----------------------------------------------------------------------+
-//
-// $Id$
-//
-// Remote service monitor
 /**
- * @package Net_Monitor
- * @author Robert Peake <robert@peakepro.com>
- * @copyright 2004
- * @license http://www.php.net/license/3_0.txt
- * @version 0.2.0
+ * Network service monitoring package
+ *
+ * A unified interface for checking the availability services on external 
+ * servers and sending meaningful alerts through a variety of media if a 
+ * service becomes unavailable.
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Net
+ * @package    Net_Monitor
+ * @author     Robert Peake <cyberscribe@php.net>
+ * @author     Bertrand Gugger <bertrand@toggg.com>
+ * @copyright  2004-2005 Robert Peake
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/PackageName
+ * @since      File available since Release 0.0.6
  */
-
 /**
  * require and extend the Net_Monitor_Service class
  */
@@ -39,6 +37,7 @@ require_once 'Net/DNS.php';
  * 
  * A class for checking DNS services
  *
+ * @category Net
  * @package Net_Monitor
  * @access public
  * @see Net_Monitor_Service
